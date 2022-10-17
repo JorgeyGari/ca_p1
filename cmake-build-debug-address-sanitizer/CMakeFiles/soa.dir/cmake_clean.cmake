520b85d5b9a50f13ddc58599dc63f1f9c9eaac81
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 <jorgelazaro2002@hotmail.com>
2022-10-17 11:56:43 +0200

    checking validity of BMP

---
+++ cmake-build-debug-address-sanitizer/CMakeFiles/soa.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/soa.dir/soa.cpp.o"
+  "CMakeFiles/soa.dir/soa.cpp.o.d"
+  "libsoa.a"
+  "libsoa.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/soa.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
