4e584592421797668407525312b0786dde905a47
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 <jorgelazaro2002@hotmail.com>
2022-10-20 16:23:50 +0200

    main function now calls read method

--- main.cpp
+++ main.cpp
@@ -1,7 +1,41 @@
 #include <iostream>
+#include<cstring>
+#include<filesystem>
 #include "soa.cpp"
 
-int main() {
-    std::filesystem::path p("../in/elephant.bmp");
-    read_bmp(p);
+using namespace std;
+
+
+int main(int argc, char *argv[]) {
+    char *in_file = argv[1];
+    char *out_file = argv[2];
+    char *op = argv[3];
+    if (argc != 4) {    /* Checks if enough arguments were provided */
+        printf("\nWrong format: \n  image  in_path  out_path  oper \n    operation: copy, histo, mono, gauss \n");
+        return -1;
+    } else if ((strcmp(op, "copy") != 0) && (strcmp(op, "histo") != 0) && (strcmp(op, "mono") != 0) &&
+               (strcmp(op, "gauss") != 0)) {
+        printf("Unexpected operation: %s \n  image  in_path  out_path  oper \n   operation: copy, histo, mono, gauss \n",
+               op);
+        return -1;
+    } else {
+        printf("\nInput path: %s \nOutput path: %s \n", in_file, out_file);
+        if (not(filesystem::exists(in_file))) {
+            printf("Cannot open directory [%s]\n  image in_path out_path oper\n    opertion: copy, histo, mono, gauss",
+                   in_file);
+            return -1;
+        } else if (not(filesystem::exists(out_file))) {
+            printf("Output directory [%s] does not exist\n  image in_path out_path oper\n    opertion: copy, histo, mono, gauss",
+                   out_file);
+            return -1;
+        } else {
+            //AQUÍ IRIA EL CÓDIGO PRINCIPAL
+            filesystem::path in_path(in_file);
+            read_bmp(in_file);  //call function to read images
+            filesystem::path out_path(out_file);
+            //execute the operations and store in output file
+
+            return 0;
+        }
+    }
 }
