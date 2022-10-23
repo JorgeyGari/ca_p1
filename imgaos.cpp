f46f1924188115e1b6c0bd78991de42b44fbda2e
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 <jorgelazaro2002@hotmail.com>
2022-10-23 18:09:17 +0200

    cherry-pick the two main .cpp's

--- imgaos.cpp
+++ imgaos.cpp
@@ -2,42 +2,11 @@
 #include<cstring>
 #include<filesystem>
 #include "aos.cpp"
-
+#include "progargs.cpp"
 
 using namespace std;
 
-
 int main(int argc, char *argv[]) {
-    char *in_file = argv[1];
-    char *out_file = argv[2];
-    char *op = argv[3];
-    if (argc != 4) {    /* Checks if enough arguments were provided */
-        printf("\nWrong format: \n  image  in_path  out_path  oper \n    operation: copy, histo, mono, gauss \n");
-        return -1;
-    } else if ((strcmp(op, "copy") != 0) && (strcmp(op, "histo") != 0) && (strcmp(op, "mono") != 0) &&
-               (strcmp(op, "gauss") != 0)) {
-        printf("Unexpected operation: %s \n  image  in_path  out_path  oper \n   operation: copy, histo, mono, gauss \n",
-               op);
-        return -1;
-    } else {
-        printf("\nInput path: %s \nOutput path: %s \n", in_file, out_file);
-        if (not(filesystem::exists(in_file))) {
-            printf("Cannot open directory [%s]\n  image in_path out_path oper\n    opertion: copy, histo, mono, gauss",
-                   in_file);
-            return -1;
-        } else if (not(filesystem::exists(out_file))) {
-            printf("Output directory [%s] does not exist\n  image in_path out_path oper\n    opertion: copy, histo, mono, gauss",
-                   out_file);
-            return -1;
-        } else {
-            //AQUÍ IRIA EL CÓDIGO PRINCIPAL
-            filesystem::path in_path(in_file);
-            Header h = read_header(in_path);
-            vector<Pixel> pixels = read_pixels(in_path, h.img_start, h.img_width, h.img_height);
-            filesystem::path out_path(out_file);
-            //execute the operations and store in output file
-
-            return 0;
-        }
-    }
+    Datastruct data;
+    argparsing(argc, argv, &data);
 }
--- imgsoa.cpp
+++ imgsoa.cpp
@@ -2,42 +2,11 @@
 #include<cstring>
 #include<filesystem>
 #include "soa.cpp"
-
+#include "progargs.cpp"
 
 using namespace std;
 
-
 int main(int argc, char *argv[]) {
-    char *in_file = argv[1];
-    char *out_file = argv[2];
-    char *op = argv[3];
-    if (argc != 4) {    /* Checks if enough arguments were provided */
-        printf("\nWrong format: \n  image  in_path  out_path  oper \n    operation: copy, histo, mono, gauss \n");
-        return -1;
-    } else if ((strcmp(op, "copy") != 0) && (strcmp(op, "histo") != 0) && (strcmp(op, "mono") != 0) &&
-               (strcmp(op, "gauss") != 0)) {
-        printf("Unexpected operation: %s \n  image  in_path  out_path  oper \n   operation: copy, histo, mono, gauss \n",
-               op);
-        return -1;
-    } else {
-        printf("\nInput path: %s \nOutput path: %s \n", in_file, out_file);
-        if (not(filesystem::exists(in_file))) {
-            printf("Cannot open directory [%s]\n  image in_path out_path oper\n    opertion: copy, histo, mono, gauss",
-                   in_file);
-            return -1;
-        } else if (not(filesystem::exists(out_file))) {
-            printf("Output directory [%s] does not exist\n  image in_path out_path oper\n    opertion: copy, histo, mono, gauss",
-                   out_file);
-            return -1;
-        } else {
-            //AQUÍ IRIA EL CÓDIGO PRINCIPAL
-            filesystem::path in_path(in_file);
-            Header h = read_header(in_path);
-            Image pixels = read_pixels(in_path, h.img_start, h.img_width, h.img_height);
-            filesystem::path out_path(out_file);
-            write_bmp(out_path, h, pixels); //execute the operations and store in output file
-
-            return 0;
-        }
-    }
+    Datastruct data;
+    argparsing(argc, argv, &data);
 }
