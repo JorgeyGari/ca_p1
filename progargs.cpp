8de0156ecc85687997d3883d0db7de019e1c4ba5
ccarmona02 <82317112+ccarmona02@users.noreply.github.com>
2022-10-25 16:44:16 +0200

    progargs.cpp
    New version with the error of parameter passing corrected and implemented a simple optimization as for the return of the function.

--- progargs.cpp
+++ progargs.cpp
@@ -1,8 +1,7 @@
-/* Source file containing the code that handles argument parsing */
-
 #include <iostream>
 #include<cstring>
 #include<filesystem>
+#include<string>
 
 using namespace std;
 
@@ -11,35 +10,35 @@ struct Datastruct{
 };
 
 
-int argparsing(int argc, char *argv[], struct Datastruct*data){
-    char *in_file = argv[1];
-    char *out_file = argv[2];
-    char *op = argv[3];
-    if (argc != 4) {    /* Checks if enough arguments were provided */
-        printf("\nWrong format: \n  image  in_path  out_path  oper \n    operation: copy, histo, mono, gauss \n");
-        return -1;
-    } else if ((strcmp(op, "copy") != 0) && (strcmp(op, "histo") != 0) && (strcmp(op, "mono") != 0) &&
-               (strcmp(op, "gauss") != 0)) {
+struct Datastruct argparsing(string in, string out, string oper){
+	
+	Datastruct data;
+	const char *in_file= in.c_str();
+	const char *out_file=out.c_str();
+	const char *op=oper.c_str();
+   
+     if ((strcmp(op, "copy") != 0) && (strcmp(op, "histo") != 0) && (strcmp(op, "mono") != 0) && (strcmp(op, "gauss") != 0)) {
         printf("Unexpected operation: %s \n  image  in_path  out_path  oper \n   operation: copy, histo, mono, gauss \n",
                op);
-        return -1;
+        exit(-1);
     } else {
         printf("\nInput path: %s \nOutput path: %s \n", in_file, out_file);
         if (not(filesystem::exists(in_file))) {
             printf("Cannot open directory [%s]\n  image in_path out_path oper\n    opertion: copy, histo, mono, gauss \n",
                    in_file);
-            return -1;
+            exit(-1);
         } else if (not(filesystem::exists(out_file))) {
          	 printf("Output directory [%s] does not exist\n  image in_path out_path oper\n    opertion: copy, histo, mono, gauss \n",
                    out_file);
-            	return -1;
+            	exit(-1);
         } else {
             filesystem::path in_path(in_file);
-            data->in= in_path;
+            data.in= in_path;
             filesystem::path out_path(out_file);
-            data->out=out_path;
+            data.out=out_path;
+	    return data;
 
-            return 0;
         }
     }
 }
+
