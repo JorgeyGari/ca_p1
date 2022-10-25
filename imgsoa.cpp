47cc1be537ad08e44d908ac2b2a7ea1f4310e57a
ccarmona02 <82317112+ccarmona02@users.noreply.github.com>
2022-10-25 16:56:11 +0200

    imgsoa.cpp
    Version of the main that analyzes the arguments and iterates over the elements of a directory. In each iteration, the program will read the image, perform the operation and finally write the file in the output file.

--- imgsoa.cpp
+++ imgsoa.cpp
@@ -1,4 +1,4 @@
-/* Main source file for the SOA version */
+/* Main source file for the AOS version */
 
 #include "soa.cpp"
 #include "progargs.cpp"
@@ -6,10 +6,18 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    Datastruct data;
-    argparsing(argc, argv, &data);
-
-    Header header = read_header(data.in);
+    if (argc != 4) { /* Checks if enough arguments were provided */
+        printf("\nWrong format: \n  image  in_path  out_path  oper \n    operation: copy, histo, mono, gauss \n");
+        exit(-1);
+    }
+	Datastruct data_files =argparsing(string(argv[1]),string(argv[2]),string(argv[3])); //analyzes if data provided is valid
+											    //
+//we want to iterate over the elements of the directory 
+    filesystem::directory_iterator it(data_files.in);
+    for (auto& entry : it){
+            cout <<"File: "<<  entry << "\n"; //TAMBIÉN HAY QUE IMPRIMIR EL TIME DE CADA COSA
+    Header header = read_header(data_files.in);
     Image image = read_pixels(data.in, header.img_start, header.img_width, header.img_height);
-    write_bmp(data.out, header, image);
+    write_bmp(data_files.out, header, image);
+	}
 }
